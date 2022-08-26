06c0f722b534e88a2ab2c073ee79ff52a3dec3c8
masudfcs1 <masudfcs1@gmail.com>
2022-08-26 22:14:49 +0600

    Add line

--- First/app.js
+++ First/app.js
@@ -5,3 +5,35 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'I am not Route'
+//     })
+// })
+
+// app.get('/', function(req, res) {
+//     console.log("Cookies: ", req.cookies)
+// })
+// app.listen(8081)
+
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrParser: true });
+
+.then(() => {
+    app.listen(4545, () => {
+        console.log("Application is ready.")
+    })
+})
+
+// .then(() => {
+//     app.listen(4545, () => {
+//         console.log('Application For Ready.');
+//     })
+// })
+
+// .catch(e => {
+//     console.log(e);
+// })
