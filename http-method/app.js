862aa000217acb6a6058671da8b37dd465421b29
masudfcs1 <masudfcs1@gmail.com>
2022-08-31 08:01:13 +0600

    add line

--- http-method/app.js
+++ http-method/app.js
@@ -2,4 +2,26 @@ const express = require('express');
 const app = express();
 const hostname = '127.0.0.1';
 
+app.get("/", (req, res) => {
+    res.send("get-Akash")
+})
+
+
+
+app.post("/", (req, res) => {
+    res.send("post_Akash")
+})
+
+
+
+app.delete("/", (req, res) => {
+    res.send(" Delete-Akash")
+})
+
+
+app.put("/", (req, res) => {
+    res.send("put-Akash")
+})
+
+
 module.exports = app;
